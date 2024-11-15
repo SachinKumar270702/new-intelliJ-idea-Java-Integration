4c37cc684b4ae17a15c2a1bfd4d9706f7f4e94fa
learning Git <sachinkumar270702@gmail.com>
2024-11-15 23:03:45 +0530

    done questions on sorting

--- basicsortingAlgo/ques2.java
+++ basicsortingAlgo/ques2.java
@@ -1,7 +1,32 @@
 package basicsortingAlgo;
 
 public class ques2 {
+    static void display(int[] arr){
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+    static void partition(int[] arr){
+        int l=0,r=arr.length-1;
+        while(l<r){
+            while(arr[l]<0) l++;
+            while(arr[r]>=0) r--;
+            if(l<r){
+                int temp=arr[l];
+                arr[l]=arr[r];
+                arr[r]=temp;
+                l++;
+                r--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        int[] nums={-13,20,7,0,-4,-13,11,-5,-13};
+        partition(nums);
+        display(nums);
 
     }
 }
