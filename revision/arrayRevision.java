8bc60fd974a95b029bec950bb0564286da41390a
learning Git <sachinkumar270702@gmail.com>
2025-01-27 00:18:48 +0530

    revsion

--- revision/arrayRevision.java
+++ revision/arrayRevision.java
@@ -2,7 +2,7 @@ package revision;
 
 public class arrayRevision {
 
-        static public void printSubArray(int[] arr){
+        static public void printSubArr(int[] arr){
             int n=arr.length;
             for(int i=0;i<n;i++){
                 for(int j=i;j<n;j++){
